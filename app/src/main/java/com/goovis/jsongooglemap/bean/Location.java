b15e5e9e9744b2429b66518f7d4640de5754bb6c
Ranjith Subramaniam <ranjith.subraman@gmail.com>
2017-03-31 11:31:25 -0500

    Removed unwanted setter methods.

--- app/src/main/java/com/goovis/jsongooglemap/bean/Location.java
+++ app/src/main/java/com/goovis/jsongooglemap/bean/Location.java
@@ -40,31 +40,16 @@ public class Location {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public double getLongitude() {
         return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public String getTime() {
         return time;
     }
 
-    public void setTime(String time) {
-        this.time = time;
-    }
 }
